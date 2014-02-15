fb95f87a3de3154cac11660d3fc0c7471b0f5a97
vagrant <vagrant@precise64.(none)>
2014-02-15 13:22:38 +0000

    fixed height for scroll

--- app/assets/javascripts/scroll.js
+++ app/assets/javascripts/scroll.js
@@ -1,7 +1,7 @@
 jQuery(document).ready(function(){
   jQuery('a.head').click(function(e) {
 
-      jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top}, 1000);
+      jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - jQuery('#header').height()}, 1000);
 
       return false;
 

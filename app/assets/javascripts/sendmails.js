c7e1588fd3a91714f6355db65f96549c2e3c2515
vagrant <vagrant@precise64.(none)>
2014-02-20 09:06:32 +0000

    Fixed responvie

--- app/assets/javascripts/sendmails.js
+++ app/assets/javascripts/sendmails.js
@@ -164,3 +164,17 @@ jQuery(document).ready(function() {
     }
   });
 });
+
+jQuery(document).ready(function(){
+  jQuery('a.head').click(function(e) {
+
+      jQuery('html,body').animate({ scrollTop: jQuery(this.hash).offset().top - jQuery('#header').height()}, 500);
+
+      return false;
+
+      e.preventDefault();
+
+  });
+});
+
+
